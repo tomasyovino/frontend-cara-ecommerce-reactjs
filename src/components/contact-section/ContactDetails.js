2c55bb7bd17c3c25b9bcd1dffc1e053a82075293
tomasyovino <tomasyovino@gmail.com>
2023-01-04 20:17:36 -0300

    Display Contact page complete design structure

--- src/components/contact-section/ContactDetails.js
+++ src/components/contact-section/ContactDetails.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 const ContactDetails = () => {
   return (
-    <div>ContactDetails</div>
+    <section id="contact-details" className="contact-details section-p1">
+        <div className="details">
+            <span>CONTÁCTANOS</span>
+            <h2>Visita una de nuestras agencias o contáctanos hoy mismo</h2>
+            <h3>Sede Central</h3>
+            <div>
+                <li>
+                    <i className="fa fa-map-o" aria-hidden="true"></i>
+                    <p>S6 General Paz, Glasgow G1 1UL - Buenos Aires</p>
+                </li>
+                <li>
+                    <i className="fa fa-envelope-o" aria-hidden="true"></i>
+                    <p>contacto@ejemplo.com</p>
+                </li>
+                <li>
+                    <i className="fa fa-phone" aria-hidden="true"></i>
+                    <p>+12 3456789101</p>
+                </li>
+                <li>
+                    <i className="fa fa-clock-o" aria-hidden="true"></i>
+                    <p>10:00 - 18:00, Lun - Sab</p>
+                </li>
+            </div>
+        </div>
+        <div className="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.064054033765!2d-58.48495404957663!3d-34.62782156618417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc994e1ad733b%3A0xc547518c9b089649!2sOxford%20College!5e0!3m2!1ses-419!2sar!4v1661368711895!5m2!1ses-419!2sar" title='hola' width="600" height="450" style={{width: "600px", height: "450px", border: 0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
   )
 }
 
--- src/components/contact-section/ContactForm.js
+++ src/components/contact-section/ContactForm.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import people1 from "../../assets/img/people/1.png";
+import people2 from "../../assets/img/people/2.png";
+import people3 from "../../assets/img/people/3.png";
 
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
+    <section id="form-details" className='form-details'>
+        <form action="">
+            <span>DÉJANOS UN MENSAJE</span>
+            <h2>Nos encanta saber de ti</h2>
+            <input type="text" placeholder="Tu nombre" />
+            <input type="email" placeholder="Tu E-mail" />
+            <input type="text" placeholder="Asunto" />
+            <textarea name="" id="" cols="30" rows="10" placeholder="Escribe tu mensaje"></textarea>
+            <button class="normal">Enviar</button>
+        </form>
+        <div class="people">
+            <div>
+                <img src={people1} alt="people" />
+                <p><span>John Doe</span> Director de Publicidad <br /> Tel: +12 3456789101 <br /> E-mail: contacto@ejemplo.com</p>
+            </div>
+            <div>
+                <img src={people2} alt="people" />
+                <p><span>William Smith</span> Director de Publicidad <br /> Tel: +12 3456789101 <br /> E-mail: contacto@ejemplo.com</p>
+            </div>
+            <div>
+                <img src={people3} alt="people" />
+                <p><span>Emma Stone</span> Director de Publicidad <br /> Tel: +12 3456789101 <br /> E-mail: contacto@ejemplo.com</p>
+            </div>
+        </div>
+    </section>
   )
 }
 
