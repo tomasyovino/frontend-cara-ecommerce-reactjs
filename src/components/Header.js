0a9822d35e0f635a0ac57cbbb9bf40c5c70de444
tomasyovino <tomasyovino@gmail.com>
2023-01-04 15:56:51 -0300

    Add Header component structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import { NavLink, Link } from "react-router-dom";
+import Logo from "../assets/img/logo.png";
 
 const Header = () => {
+  
   return (
-    <div>Header</div>
+    <header id="header" className='header'>
+       <Link to="/"><img src={Logo} class="logo" alt="logo" /></Link> 
+
+       <div>
+            <ul id="navbar" className='navbar'>
+                <li>
+                  <NavLink
+                    to="/" 
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                  >
+                    Inicio
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/shop"
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                  >
+                    Tienda
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/blog"
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                  >
+                    Blog
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/about"
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                  >
+                    Sobre Nosotros
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/contact"
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                  >
+                    Contáctanos
+                  </NavLink>
+                </li>
+                <li id="lg-bag">
+                  <NavLink 
+                    to="/cart"
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                  >
+                    <i class="fa-solid fa-cart-shopping"></i>
+                  </NavLink>
+                </li>
+                <span id="close"><i class="fa fa-times"></i></span>
+            </ul>
+       </div>
+       <div id="mobile" className='mobile'>
+            <NavLink to="/cart"><i class="fa-solid fa-cart-shopping"></i></NavLink>
+            <i id="bar" class="fas fa-outdent"></i>
+       </div>
+    </header>
   )
 }
 
