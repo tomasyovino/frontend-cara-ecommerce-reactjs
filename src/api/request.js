1f9ea3bbe4d4f4bcd2aa7ad1d8d3e4053bb84246
tomasyovino <tomasyovino@gmail.com>
2023-01-15 21:11:41 -0300

    Change hardcoded token for a Local Storage token

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYjhjNTA4MzM5NzI2NjAxODE4ODFlOSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MzcyODg0NywiZXhwIjoxNjczOTg4MDQ3fQ.TGpaL9ulaPbcSWpMTu6pu17M50bjXBF3q6e91n82h0A";
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL
